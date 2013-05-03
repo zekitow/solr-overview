49358ab2e2b40a2f1d1c9cbf932131406770f763
Jose Ribeiro <jose.ribeiro@pitang.com>
2013-05-03 15:21:32 -0300

    Creating simple search

--- design/solr-webapp/webapp/solr.js
+++ design/solr-webapp/webapp/solr.js
@@ -3,11 +3,14 @@ var HOST = 'http://localhost:8983/solr/products';
 Solr = {
   commands: {
     full_import: HOST + '/dataimport?command=full-import&clean=false&commit=true&wt=json&indent=true',
-    status: HOST + '/dataimport?command=status&wt=json'
+    status: HOST + '/dataimport?command=status&wt=json',
+    find: HOST + '/select?wt=json&indent=true&q='
+    
   }
 }
 
 $(document).ready(function() {
+  jQuery.fn.exists = function(){return this.length>0;}
   thead = function(data) { return '<th>' + data + '</th>' }
   tbody = function(data) { return '<td>' + data + '</td>' }
   
@@ -24,7 +27,7 @@ $(document).ready(function() {
     $.ajax({
       url: Solr.commands.full_import
     }).done(function(data) {
-      $('div.status').toggleClass('hide');
+      $('div.status').removeClass('hide');
       $('div.status').html("<b>Indexando...</b>");
       
       $('table thead').append('<tr>');
@@ -46,8 +49,35 @@ $(document).ready(function() {
     $.ajax({
       url: Solr.commands.full_import
     }).done(function(data) {
-      $('div.status').toggleClass('hide');
+      $('div.status').removeClass('hide');
       $('div.status').html("<b>Status: </b>" + data["status"]);
     });
   });
+  
+  $("#search").click(function() {
+    clear();
+    term = '*'
+    if ($('#keyword').val() != '') term = $('#keyword').val();
+    
+    var q = term + '*';
+    $.ajax({
+      url: Solr.commands.find + q
+    }).done(function(data) {
+      $('table thead').append('<tr>');
+      $('table thead tr').append(thead("Categoria"));
+      $('table thead tr').append(thead("Nome"));
+      $('table thead tr').append(thead("Preco"));
+      $('table thead').append('</tr>');
+  
+      var body = '';
+      for (var item in data.response.docs) {
+        body += ('<tr>');
+        body += (tbody(data.response.docs[item]["category"]));
+        body += (tbody(data.response.docs[item]["product_name"]));
+        body += (tbody(data.response.docs[item]["price"]));
+        body += ('</tr>');
+      }
+      $('table tbody').append(body);
+    });
+  });  
 })
